905a8545053fc12cd6aa98bf055b342b4ab4dfb2
andydinh <andydinh2703@gmail.com>
2025-12-19 12:18:01 -0500

    fix projects.ts to use import intead of filesystem read

--- src/lib/server/projects.ts
+++ src/lib/server/projects.ts
@@ -1,21 +1,15 @@
-import { readFile, writeFile } from 'fs/promises';
-import { join } from 'path';
-
-// declare data file
-const DATA_FILE = join(process.cwd(), 'src/data/projects.json');
-
 export interface ProjectMeta {
     title: string;
     description: string;
     imageUrl: string;
     techStack: string[];
     projectUrl: string;
-    date:string;
+    date: string;
 }
 
-// get project function
+// get project function - uses import so it works on Vercel
 export async function getProjects(): Promise<ProjectMeta[]> {
-    const data = await readFile(DATA_FILE, 'utf-8');
-    return JSON.parse(data).projects;
+    const data = await import('$lib/../data/projects.json');
+    return data.projects;
 }
 
